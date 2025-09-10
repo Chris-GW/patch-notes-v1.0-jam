1630a841e968fb9fb54bbcc2662874469d442ceb
Chris-GW <Chris-GW@users.noreply.github.com>
2025-09-10 20:25:14 +0200

    add patch notes logo

--- main.gd
+++ main.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+@export var rotation_speed: float
+
+@onready var godot_icon: TextureRect = $CanvasLayer/GodotIcon
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +12,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	godot_icon.rotation += rotation_speed * delta
