828fc59f9f00e24e0b5297376cdf10ca78426bb4
Chris-GW <Chris-GW@users.noreply.github.com>
2025-10-05 14:43:00 +0200

    screenshot scene which is used for create background and cover image

---
+++ levels/level_bootscreen.gd
@@ -0,0 +1,41 @@
+extends Node2D
+
+@export_dir var save_dir: String
+
+@onready var camera_2d: Camera2D = %Camera2D
+@onready var sub_viewport: SubViewport = $SubViewportContainer/SubViewport
+
+
+func _ready() -> void:
+	%MenuPanelContainer.visible = false
+	%RunGameButton.visible = false
+	await get_tree().process_frame
+	for node in get_tree().get_nodes_in_group("reparent_subviewport"):
+		node.reparent.call_deferred(sub_viewport)
+	await get_tree().process_frame
+	take_screen_shots.call_deferred()
+
+
+func take_screen_shots() -> void:
+	for screenshot_rect in get_tree().get_nodes_in_group("screenshot_rect"):
+		await take_screen_shot(screenshot_rect)
+	get_tree().quit()
+
+
+func take_screen_shot(reference_rect: ReferenceRect) -> void:
+	sub_viewport.size = reference_rect.size
+	camera_2d.position = reference_rect.position
+	%GameTitleContainer1.visible = reference_rect.name != "îtchio_website_background"
+	%GameTitleContainer2.visible = reference_rect.name == "îtchio_website_background"
+	%GameTitleContainer3.visible = reference_rect.name == "îtchio_website_background"
+	await get_tree().process_frame
+	
+	var img_name := reference_rect.name
+	var img: Image = sub_viewport.get_texture().get_image()
+	# Save screenshot as PNG
+	var path := save_dir + "/%s.png" % img_name
+	var err := img.save_png(path)
+	if err == OK:
+		print("Screenshot saved to: %s" % path)
+	else:
+		push_error("Failed to save screenshot: %s" % path)
